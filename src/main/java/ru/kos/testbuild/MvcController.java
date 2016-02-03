8ddd15bf3bd7695f6c8c38982dbe614ce27677f5
brjazgin <k.brjazgin@ftc.ru>
2016-02-03 13:40:41 +0600

    thymeleaf param

--- src/main/java/ru/kos/testbuild/MvcController.java
+++ src/main/java/ru/kos/testbuild/MvcController.java
@@ -1,7 +1,9 @@
 package ru.kos.testbuild;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by brjazgin on 03.02.2016.
@@ -9,7 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MvcController {
     @RequestMapping("/")
-    public String greeting() {
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
         return "greeting";
     }
 }
